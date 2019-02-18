140d8a9bd4bcd07e7e190fa5cac0e32d04165b8d
George Korolev <polecater@mail.ru>
2019-02-18 23:58:30 +0500

    Implement piece of InputOutputModule

--- PascalCompiler/InputOutputModule.cs
+++ PascalCompiler/InputOutputModule.cs
@@ -1,16 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PascalCompiler
 {
-    public class InputOutputModule
+    public class InputOutputModule : IDisposable
     {
-        public void GetNextCharacter()
+        public StreamReader InputStream { get; private set; }
+        public StreamWriter OutputStream { get; private set; }
+
+        public int CurrentLineNumber { get; private set; }
+        public int CurrentCharacterNumber { get; private set; }
+
+        public string CurrentLine { get; private set; }
+        public string CurrentCharacter { get; private set; }
+
+        public InputOutputModule(string inputFileName, string outputFileName)
+        {
+            InputStream = new StreamReader(inputFileName);
+            OutputStream = new StreamWriter(outputFileName);
+            CurrentLineNumber = 0;
+            CurrentCharacterNumber = 0;
+        }
+
+        public char GetNextCharacter()
+        {
+            CurrentLine = InputStream.ReadLine();
+
+            if (CurrentCharacterNumber == CurrentLine.Length)
+            {
+                
+            }
+
+            return 'a';
+        }
+
+        public void InsertError(int line, int column)
         {
 
+        }
+
+        public void Dispose()
+        {
+            InputStream?.Dispose();
+            OutputStream?.Dispose();
         }
     }
 }
