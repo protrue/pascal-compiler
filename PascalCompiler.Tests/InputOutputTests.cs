5d8383ce8383427701720ec0885fa74a1c28b0be
protrue <polecater@mail.ru>
2019-02-19 22:06:03 +0500

    Implement GetNextCharacter

--- PascalCompiler.Tests/InputOutputTests.cs
+++ PascalCompiler.Tests/InputOutputTests.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FluentAssertions;
 
@@ -8,9 +11,21 @@ namespace PascalCompiler.Tests
     public class InputOutputTests
     {
         [TestMethod]
-        public void InputOutputModuleShouldWork()
+        public void InputOutputModuleGetCharacterTest()
         {
-            (true).Should().Be(true);
+            var inputFileName = "input.txt";
+            var inputOutputModule = new InputOutputModule(inputFileName, "output.txt");
+
+            var stringBuilder = new StringBuilder();
+
+            while (!inputOutputModule.IsEndOfFile)
+                stringBuilder.Append(inputOutputModule.GetNextCharacter());
+
+            var rawText = string.Join(string.Empty, File.ReadAllLines(inputFileName));
+            var readText = stringBuilder.ToString();
+
+            rawText.Should().BeEquivalentTo(readText);
         }
     }
 }
+
--- PascalCompiler/InputOutputModule.cs
+++ PascalCompiler/InputOutputModule.cs
@@ -9,29 +9,46 @@ namespace PascalCompiler
 {
     public class InputOutputModule : IDisposable
     {
-        public StreamReader InputStream { get; private set; }
-        public StreamWriter OutputStream { get; private set; }
+        public StreamReader InputStream { get; }
+        public StreamWriter OutputStream { get; }
 
         public int CurrentLineNumber { get; private set; }
         public int CurrentCharacterNumber { get; private set; }
 
         public string CurrentLine { get; private set; }
-        public string CurrentCharacter { get; private set; }
+        public char CurrentCharacter { get; private set; }
+
+        public bool IsEndOfFile => InputStream.EndOfStream && CurrentCharacterNumber >= CurrentLine.Length - 1;
 
         public InputOutputModule(string inputFileName, string outputFileName)
         {
             InputStream = new StreamReader(inputFileName);
             OutputStream = new StreamWriter(outputFileName);
-            CurrentLineNumber = 0;
-            CurrentCharacterNumber = 0;
+
+            CurrentLineNumber = -1;
+            CurrentCharacterNumber = -1;
+
+            CurrentLine = string.Empty;
         }
 
         public char GetNextCharacter()
         {
-            throw new NotImplementedException();
+            if (CurrentCharacterNumber >= CurrentLine.Length - 1 || CurrentLineNumber == -1)
+            {
+                if (IsEndOfFile)
+                    throw new EndOfStreamException("Невозможно получить следующую литеру: достигнут конец файла");
+
+                CurrentLine = InputStream.ReadLine();
+                CurrentLineNumber++;
+                CurrentCharacterNumber = -1;
+            }
+
+            CurrentCharacter = CurrentLine[++CurrentCharacterNumber];
+
+            return CurrentCharacter;
         }
 
-        public void InsertError(int line, int column)
+        public void InsertError()
         {
             throw new NotImplementedException();
         }
